3b6b27b648ba6a431747aa2fa71aa0ea3729b404
Luka Glusica <luka.glusica89@gmail.com>
2017-06-23 02:46:49 +0200

    Update gh-pages

--- app/app.js
+++ app/app.js
@@ -1,4 +1,17 @@
 angular.module('app', ['ngAnimate', 'ui.bootstrap', 'ui.router'])
-	.controller('AppController', function () {
+	.controller('AppController', function() {
 		var app = this;
-	});
+	})
+
+	.config(function($stateProvider) {
+		var classState = {
+			name: 'class',
+			url: '/class/{className}',
+			params: {
+				className: null
+			},
+			templateUrl: 'app/partials/content.html'
+		}
+
+		$stateProvider.state(classState);
+	})
--- app/controllers/ClassListController.js
+++ app/controllers/ClassListController.js
@@ -1,8 +1,29 @@
 angular.module('app')
-	.controller('ClassListController', function() {
+	.controller('ClassListController', function($state) {
 		var list = this;
 
 		list.classes = [
+			'istsos.ServerContainer',
+			'istsos.Date',
+			'istsos.Database',
+			'istsos.Configuration',
+			'istsos.Server',
+			'istsos.Service',
+			'istsos.Offering',
+			'istsos.ProcedureBase',
+			'istsos.Procedure',
+			'istsos.VirtualProcedure',
+			'istsos.Output',
+			'istsos.ObservedProperty',
+			'istsos.DataQuality',
+			'istsos.UnitOfMeasure',
+			'istsos.EventTypes',
+			'istsos.EventEmitter',
+			'istsos.HttpAPI'
+		];
 
-		]
+		list.goTo = function(state, param) {
+			console.log(param)
+			$state.go(state, {className: param})
+		}
 	})
